073423015d16a815004409c199de5838c0335db9
Raghu Rayamajhi <ryamacoding@outlook.com>
2024-10-24 21:09:09 +0100

    made a spirograph

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
-from turtle import Turtle, Screen
+import turtle as t
 import random
 
-tim = Turtle()
+tim = t.Turtle()
+t.colormode(255)
 
-colors = ["Blue", "Red", "Orange", "Green", "Purple", "Yellow"]
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color = (r, g, b)
+    return color
 
-def draw_random(steps):
-    angle = [0, 90, 180, 270]
-    for _ in range(steps):
-        tim.forward(30)
-        tim.right(random.choice(angle))
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360 / size_of_gap)):
+        tim.speed("fastest")
+        tim.color(random_color())
+        tim.circle(100)
+        tim.setheading(tim.heading() + size_of_gap)
 
-for _ in range(10):
-    tim.speed("fastest")
-    tim.pensize(15)
-    tim.color(random.choice(colors))
-    draw_random(_)
+draw_spirograph(5)
 
-
-screen = Screen()
+screen = t.Screen()
 screen.exitonclick()
+
 
